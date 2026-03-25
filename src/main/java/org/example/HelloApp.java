5bdc0ba7abb28efa2678793d048b000305938c10
priya2612-hub <priyaramamoorthy2612@gmail.com>
2026-03-25 09:18:35 +0530

    [Priya Ramamoorthy] added: Java code for UC4

--- src/main/java/org/example/HelloApp.java
+++ src/main/java/org/example/HelloApp.java
@@ -2,8 +2,13 @@ package org.example;
 
 public class HelloApp {
     public static void main(String[] args) {
-        //UC3
-        String name = (args.length > 0) ? args[0] : "World";
-        System.out.println("Hello, " + name + "!");
+        //UC4
+        String names;
+        if (args.length > 0) {
+            names = String.join(", ", args);
+        } else {
+            names = "World";
+        }
+        System.out.println("Hello, " + names + "!");
     }
 }
