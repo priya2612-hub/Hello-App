17b8102c5c3a3161cf56a10815079abcfa41b6bd
priya2612-hub <priyaramamoorthy2612@gmail.com>
2026-03-25 19:04:57 +0530

    [Priya Ramamoorthy] added: Java code for UC6

--- src/main/java/org/example/HelloApp.java
+++ src/main/java/org/example/HelloApp.java
@@ -1,14 +1,25 @@
 package org.example;
 
 public class HelloApp {
+
     public static void main(String[] args) {
-        //UC4
-        String names;
-        if (args.length > 0) {
-            names = String.join(", ", args);
+
+        // Case 1: No arguments
+        if (args.length == 0) {
+            System.out.println("Hello, World!");
         } else {
-            names = "World";
+
+            String names = "";
+
+            // Append all names with comma
+            for (int i = 0; i < args.length; i++) {
+                names += args[i] + ", ";
+            }
+
+            // Remove last ", " using substring
+            names = names.substring(0, names.length() - 2);
+
+            System.out.println("Hello, " + names + "!");
         }
-        System.out.println("Hello, " + names + "!");
     }
 }
