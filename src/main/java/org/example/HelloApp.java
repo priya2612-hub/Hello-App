c327dfaaebf157af0cd0edd1c4d475d7c667e5f5
priya2612-hub <priyaramamoorthy2612@gmail.com>
2026-03-25 08:58:51 +0530

    [Priya Ramamoorthy] added: Java code for UC2

--- src/main/java/org/example/HelloApp.java
+++ src/main/java/org/example/HelloApp.java
@@ -1,7 +1,12 @@
 package org.example;
 
 public class HelloApp {
-    public static void main(String[] args){
-        System.out.println("Hello World");
+    public static void main(String[] args) {
+        //UC2
+        if (args.length > 0) {
+            System.out.println("Hello, " + args[0] + "!");
+        } else {
+            System.out.println("Hello, World!");
+        }
     }
 }
